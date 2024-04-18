4df091e27585afc34ace07d384cb847eff2e4d2a
Rahad-Bin-Al Islam <rahadbinalislam@gmail.com>
2024-04-19 03:41:41 +0600

    added toast

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,7 +2,13 @@ import React from "react";
 import user from "./../../images/user.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./Dashboard.css";
+
+//toast.configure();
+
+const notify = () => toast("Successfully Completed Today's Exercise!");
 
 const Dashboard = ({ dashboard }) => {
   let time = 0;
@@ -23,29 +29,37 @@ const Dashboard = ({ dashboard }) => {
         </div>
       </div>
       <div className="metrics">
-        <h3>
+        <h4>
           75<small>kg</small>
           <br />
           <small>Weight</small>
-        </h3>
-        <h3>
+        </h4>
+        <h4>
           6.5<small>in</small>
           <br />
           <small>Height</small>
-        </h3>
-        <h3>
+        </h4>
+        <h4>
           25<small>yrs</small>
           <br />
           <small>Age</small>
-        </h3>
+        </h4>
       </div>
-      <h2>Add A Break</h2>
-      <h2>Exercise Details</h2>
-      <h3>Exercise Time: {time} seconds</h3>
-      <h3>Break time: seconds</h3>
-      {/* <button className="btn-cart">
+      <h3>Add A Break</h3>
+      <div className="timer">
+        <button>10s</button>
+        <button>20s</button>
+        <button>30s</button>
+        <button>40s</button>
+        <button>50s</button>
+      </div>
+      <h3>Exercise Details</h3>
+      <h5 className="spacing">Exercise Time: {time} seconds</h5>
+      <h5 className="spacing">Break time: seconds</h5>
+      <button className="completion" onClick={notify}>
         <p>Activity Completed</p>
-      </button> */}
+      </button>
+      <ToastContainer />
     </div>
   );
 };
